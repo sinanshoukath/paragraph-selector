5b7c1ee1217acd681d1f2207094fbbd1b8abf367
sinan <sinan.shoukath@gmail.com>
2013-02-01 16:53:28 +0530

    Searching of text keywords have been implemented.

--- ParagraphSelector/src/com/android/paragraphselector/MainActivity.java
+++ ParagraphSelector/src/com/android/paragraphselector/MainActivity.java
@@ -1,11 +1,16 @@
 package com.android.paragraphselector;
 
-import android.os.Bundle;
+import java.lang.reflect.Method;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 /*
@@ -13,12 +18,13 @@ import android.widget.Spinner;
  * @author Sinan Shoukath
  */
 public class MainActivity extends Activity {
+  private WebView webView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    final WebView webView = (WebView)findViewById(R.id.webview);
+    webView = (WebView)findViewById(R.id.webview);
     final ProgressDialog dialog = ProgressDialog.show(this, "Paragraph Selector", "Loading data");
     webView.loadUrl("file:///android_asset/input_text.txt");
     webView.setWebViewClient(new WebViewClient() {
@@ -27,11 +33,33 @@ public class MainActivity extends Activity {
         dialog.dismiss();
       }
     });
-    Spinner spinner = (Spinner)findViewById(R.id.word_spinner);
+    final Spinner spinner = (Spinner) findViewById(R.id.word_spinner);
     String[] array_spinner = {"fixed", "drive", "car", "surrounding", "little", "property", "para",
         "bus", "trust", "person"};
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-        android.R.layout.simple_spinner_item, array_spinner);
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.
+         simple_spinner_item, array_spinner);
     spinner.setAdapter(adapter);
+    Button searchButton = (Button)findViewById(R.id.search);
+    searchButton.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        searchText(spinner.getSelectedItem().toString());
+      }
+    });
+  }
+
+  @SuppressWarnings("deprecation")
+  private void searchText(String text) {
+    webView.findAll(text);
+    webView.setSelected(true);
+    webView.findNext(true);
+      try {
+        for(Method m : WebView.class.getDeclaredMethods()) {
+            if(m.getName().equals("setFindIsUp")) {
+                m.setAccessible(true);
+                m.invoke(webView, true);
+            }
+        }
+      }catch(Exception ignored){}
   }
 }
