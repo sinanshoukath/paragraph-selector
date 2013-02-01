0b5b1e42c7dbef5e6fd293641486f0e5c0acc642
sinan <sinan.shoukath@gmail.com>
2013-02-01 17:33:18 +0530

    webView declared and initialised inside onCreate and passed to searchText method.

--- ParagraphSelector/src/com/android/paragraphselector/MainActivity.java
+++ ParagraphSelector/src/com/android/paragraphselector/MainActivity.java
@@ -18,13 +18,12 @@ import android.widget.Spinner;
  * @author Sinan Shoukath
  */
 public class MainActivity extends Activity {
-  private WebView webView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    webView = (WebView)findViewById(R.id.webview);
+    final WebView webView = (WebView)findViewById(R.id.webview);
     final ProgressDialog dialog = ProgressDialog.show(this, "Paragraph Selector", "Loading data");
     webView.loadUrl("file:///android_asset/input_text.txt");
     webView.setWebViewClient(new WebViewClient() {
@@ -36,30 +35,31 @@ public class MainActivity extends Activity {
     final Spinner spinner = (Spinner) findViewById(R.id.word_spinner);
     String[] array_spinner = {"fixed", "drive", "car", "surrounding", "little", "property", "para",
         "bus", "trust", "person"};
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.
-         simple_spinner_item, array_spinner);
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        android.R.layout.simple_spinner_item, array_spinner);
     spinner.setAdapter(adapter);
     Button searchButton = (Button)findViewById(R.id.search);
     searchButton.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
-        searchText(spinner.getSelectedItem().toString());
+        searchText(spinner.getSelectedItem().toString(), webView);
       }
     });
   }
 
   @SuppressWarnings("deprecation")
-  private void searchText(String text) {
+  private void searchText(String text, WebView webView) {
     webView.findAll(text);
     webView.setSelected(true);
     webView.findNext(true);
-      try {
-        for(Method m : WebView.class.getDeclaredMethods()) {
-            if(m.getName().equals("setFindIsUp")) {
-                m.setAccessible(true);
-                m.invoke(webView, true);
-            }
+    try {
+      for(Method m : WebView.class.getDeclaredMethods()) {
+        if(m.getName().equals("setFindIsUp")) {
+          m.setAccessible(true);
+          m.invoke(webView, true);
         }
-      }catch(Exception ignored){}
+      }
+    }catch (Exception ignored) {
+    }
   }
 }
